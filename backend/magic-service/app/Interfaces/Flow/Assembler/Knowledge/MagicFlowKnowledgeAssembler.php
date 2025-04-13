b35e7af68fcef5f1d36b1389bf706a2667879106
lihq1403 <lihqing1403@gmail.com>
2025-04-13 17:08:32 +0800

    refactor: remove unused methods in MagicFlowKnowledgeAssembler for improved clarity

--- backend/magic-service/app/Interfaces/Flow/Assembler/Knowledge/MagicFlowKnowledgeAssembler.php
+++ backend/magic-service/app/Interfaces/Flow/Assembler/Knowledge/MagicFlowKnowledgeAssembler.php
@@ -8,65 +8,13 @@ declare(strict_types=1);
 namespace App\Interfaces\Flow\Assembler\Knowledge;
 
 use App\Domain\KnowledgeBase\Entity\KnowledgeBaseEntity;
-use App\Domain\KnowledgeBase\Entity\ValueObject\KnowledgeType;
 use App\Infrastructure\Core\ValueObject\Page;
 use App\Interfaces\Kernel\Assembler\OperatorAssembler;
 use App\Interfaces\Kernel\DTO\PageDTO;
-use App\Interfaces\KnowledgeBase\DTO\KnowledgeBaseDTO;
 use App\Interfaces\KnowledgeBase\DTO\KnowledgeBaseListDTO;
-use DateTime;
 
 class MagicFlowKnowledgeAssembler
 {
-    public static function createDTO(KnowledgeBaseEntity $magicFlowKnowledgeEntity): KnowledgeBaseDTO
-    {
-        $dto = new KnowledgeBaseDTO();
-        $dto->setId($magicFlowKnowledgeEntity->getCode());
-        $dto->setName($magicFlowKnowledgeEntity->getName());
-        $dto->setDescription($magicFlowKnowledgeEntity->getDescription());
-        $dto->setType($magicFlowKnowledgeEntity->getType()->value);
-        $dto->setEnabled($magicFlowKnowledgeEntity->isEnabled());
-        $dto->setBusinessId($magicFlowKnowledgeEntity->getBusinessId());
-        $dto->setSyncStatus($magicFlowKnowledgeEntity->getSyncStatus()->value);
-        $dto->setSyncStatusMessage($magicFlowKnowledgeEntity->getSyncStatusMessage());
-        $dto->setModel($magicFlowKnowledgeEntity->getModel());
-        $dto->setVectorDB($magicFlowKnowledgeEntity->getVectorDB());
-        $dto->setOrganizationCode($magicFlowKnowledgeEntity->getOrganizationCode());
-        $dto->setCreator($magicFlowKnowledgeEntity->getCreator());
-        $dto->setCreatedAt($magicFlowKnowledgeEntity->getCreatedAt());
-        $dto->setModifier($magicFlowKnowledgeEntity->getModifier());
-        $dto->setUpdatedAt($magicFlowKnowledgeEntity->getUpdatedAt());
-
-        $dto->setFragmentCount($magicFlowKnowledgeEntity->getFragmentCount());
-        $dto->setExpectedCount($magicFlowKnowledgeEntity->getExpectedCount());
-        $dto->setCompletedCount($magicFlowKnowledgeEntity->getCompletedCount());
-        $dto->setUserOperation($magicFlowKnowledgeEntity->getUserOperation());
-        $dto->setExpectedNum($magicFlowKnowledgeEntity->getExpectedNum());
-        $dto->setCompletedNum($magicFlowKnowledgeEntity->getCompletedNum());
-
-        return $dto;
-    }
-
-    public static function creatDO(KnowledgeBaseDTO $dto): KnowledgeBaseEntity
-    {
-        $magicFlowKnowledgeEntity = new KnowledgeBaseEntity();
-        $magicFlowKnowledgeEntity->setCode($dto->getId());
-        $magicFlowKnowledgeEntity->setName($dto->getName());
-        $magicFlowKnowledgeEntity->setDescription($dto->getDescription());
-        $type = KnowledgeType::tryFrom($dto->getType());
-        if ($type) {
-            $magicFlowKnowledgeEntity->setType($type);
-        }
-        $magicFlowKnowledgeEntity->setEnabled($dto->isEnabled());
-        $magicFlowKnowledgeEntity->setModel($dto->getModel());
-        $magicFlowKnowledgeEntity->setVectorDB($dto->getVectorDB());
-        $magicFlowKnowledgeEntity->setBusinessId($dto->getBusinessId());
-        $magicFlowKnowledgeEntity->setCreatedAt(new DateTime());
-        $magicFlowKnowledgeEntity->setExpectedNum($dto->getExpectedNum());
-        $magicFlowKnowledgeEntity->setCompletedNum($dto->getCompletedNum());
-        return $magicFlowKnowledgeEntity;
-    }
-
     /**
      * @param array<KnowledgeBaseEntity> $list
      */
