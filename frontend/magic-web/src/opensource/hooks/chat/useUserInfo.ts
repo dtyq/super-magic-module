69d3c3ac2a0043b2cf685b61a6d42939d84b5c67
seagull <61051874+Merlin218@users.noreply.github.com>
2025-04-14 09:30:03 +0800

    fix: 重构useUserInfo钩子，移除强制加载逻辑，优化ContactsSubSider组件用户信息获取方式

--- frontend/magic-web/src/opensource/hooks/chat/useUserInfo.ts
+++ frontend/magic-web/src/opensource/hooks/chat/useUserInfo.ts
@@ -1,25 +1,15 @@
-import { useEffect, useMemo, useRef } from "react"
+import { useMemo } from "react"
 import userInfoStore from "@/opensource/stores/userInfo"
-import userInfoService from "@/opensource/services/userInfo"
 import { computed } from "mobx"
 
 /**
  * 获取多个用户信息
  * @param uid 用户ID
  */
-const useUserInfo = (uid?: string | null, force: boolean = false) => {
+const useUserInfo = (uid?: string | null) => {
 	const userInfo = useMemo(() => {
 		return computed(() => (uid ? userInfoStore.get(uid) : undefined))
 	}, [uid]).get()
-
-	const forced = useRef(false)
-	useEffect(() => {
-		if (force && uid && !forced.current) {
-			userInfoService.fetchUserInfos([uid], 2).then(() => {
-				forced.current = true
-			})
-		}
-	}, [force, uid, userInfo])
 
 	return { userInfo }
 }
