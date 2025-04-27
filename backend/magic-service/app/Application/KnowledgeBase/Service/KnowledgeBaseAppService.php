d3efb4bfc368b77f2278ebc8a53c0b5696cffd74
leftzzzz <1059051242@qq.com>
2025-04-27 16:37:20 +0800

    fix: 修复报错

--- backend/magic-service/app/Application/KnowledgeBase/Service/KnowledgeBaseAppService.php
+++ backend/magic-service/app/Application/KnowledgeBase/Service/KnowledgeBaseAppService.php
@@ -80,8 +80,8 @@ class KnowledgeBaseAppService extends AbstractKnowledgeAppService
         try {
             $embeddingModel = di(ModelGatewayMapper::class)->getEmbeddingModelProxy($magicFlowKnowledgeEntity->getModel());
             $modelName = $embeddingModel->getModelName();
-            $embeddingResult = $embeddingModel->embeddings('test', businessParams: ['organization_id' => $dataIsolation->getCurrentOrganizationCode(), 'user_id' => $dataIsolation->getCurrentUserId()]);
-            if (count($embeddingResult->getEmbeddings()) !== $model->getVectorSize()) {
+            $embeddingResult = $embeddingModel->embedding('test', businessParams: ['organization_id' => $dataIsolation->getCurrentOrganizationCode(), 'user_id' => $dataIsolation->getCurrentUserId()]);
+            if (count($embeddingResult->getEmbeddings()) !== $embeddingModel->getVectorSize()) {
                 throw new BusinessException(__('flow.model.vector_size_not_match'));
             }
         } catch (Throwable $exception) {
