a90f478711052e24ddb2d7fc6f7668211055c122
leftzzzz <1059051242@qq.com>
2025-04-27 15:25:27 +0800

    modify: 召回测试接口新增文档名

--- backend/magic-service/app/Interfaces/KnowledgeBase/Facade/KnowledgeBaseFragmentApi.php
+++ backend/magic-service/app/Interfaces/KnowledgeBase/Facade/KnowledgeBaseFragmentApi.php
@@ -92,8 +92,7 @@ class KnowledgeBaseFragmentApi extends AbstractKnowledgeBaseApi
     {
         $query = $this->request->input('query', '');
         $userAuthorization = $this->getAuthorization();
-        $entities = $this->knowledgeBaseFragmentAppService->similarity($userAuthorization, $code, $query);
-        $list = array_map(fn (KnowledgeBaseFragmentEntity $entity) => KnowledgeBaseFragmentAssembler::entityToDTO($entity), $entities);
+        $list = $this->knowledgeBaseFragmentAppService->similarity($userAuthorization, $code, $query);
         return new PageDTO(1, count($list), $list);
     }
 }
