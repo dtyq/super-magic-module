d356f96dc731520ce5387907b176965c74b80565
Rock <rock@example.com>
2025-05-07 15:41:09 +0800

    更新 backend_split.sh 脚本，修改远程仓库地址并注释掉不必要的代码行

--- bin/backend_split.sh
+++ bin/backend_split.sh
@@ -24,11 +24,11 @@ if [[ $# -eq 0 ]]; then
     REPOS=$(ls $BASEPATH)
 fi
 
-remote github git@github.com:dtyq/.github.git
-split "backend/.github" "git@github.com:dtyq/.github.git"
+remote github git@github.com:dtyq/super-magic-module.git
+split "backend/super-magic-module" "git@github.com:dtyq/super-magic-module.git"
 
-for REPO in $REPOS ; do
-    remote $REPO git@github.com:dtyq/$REPO.git
+# for REPO in $REPOS ; do
+#     remote $REPO git@github.com:dtyq/$REPO.git
 
-    split "backend/$REPO" $REPO
-done
+#     split "backend/$REPO" $REPO
+# done
