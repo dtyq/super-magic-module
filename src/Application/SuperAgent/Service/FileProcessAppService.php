abd4d1a9a3720955d1f1cb604bc67b9bb7c79ecd
liaobw <liaobingwei@kkgroup.cn>
2025-05-18 23:53:40 +0800

    重构FileAppService中的getStsTemporaryCredential方法，增加对本地驱动的临时凭证处理，并在FileProcessAppService中整合使用FileAppService获取STS Token，提升代码复用性和可读性。

--- src/Application/SuperAgent/Service/FileProcessAppService.php
+++ src/Application/SuperAgent/Service/FileProcessAppService.php
@@ -364,24 +364,12 @@ class FileProcessAppService extends AbstractAppService
             $storageType = StorageBucketType::Private->value;
             $expires = 7200; // 凭证有效期2小时
 
-            // 调用文件服务获取STS Token
-            $data = $this->fileDomainService->getStsTemporaryCredential(
-                $organizationCode,
-                $storageType,
-                $workDir,
-                $expires
-            );
+            // 创建用户授权对象
+            $userAuthorization = new MagicUserAuthorization();
+            $userAuthorization->setOrganizationCode($organizationCode);
 
-            // 如果是本地驱动，那么增加一个临时 key
-            if ($data['platform'] === AdapterName::LOCAL) {
-                $localCredential = 'local_credential:' . IdGenerator::getUniqueId32();
-                $data['temporary_credential']['dir'] = $organizationCode . '/' . $data['temporary_credential']['dir'];
-                $data['temporary_credential']['credential'] = $localCredential;
-                $data['temporary_credential']['read_host'] = env('FILE_LOCAL_DOCKER_READ_HOST', '');
-                $data['temporary_credential']['host'] = env('FILE_LOCAL_DOCKER_WRITE_HOST', '');
-                $this->cache->set($localCredential, ['organization_code' => $organizationCode], (int) ($data['expires'] - time()));
-            }
-            return $data;
+            // 使用统一的FileAppService获取STS Token
+            return $this->fileAppService->getStsTemporaryCredential($userAuthorization, $storageType, $workDir, $expires);
         } catch (Throwable $e) {
             $this->logger->error(sprintf(
                 '刷新STS Token失败: %s，组织编码: %s，沙箱ID: %s',
