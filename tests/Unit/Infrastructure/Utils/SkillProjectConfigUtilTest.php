aa0ff438682c6f7fcc49e8a6751f104acb1e0a8b
liaobw <liaobingwei@dtyq.com>
2026-03-27 17:19:35 +0800

    test(utils): update skill project config util test assertions
    - Add @internal annotation to test class
    - Fix escaped quote handling in YAML string assertions
    - Correct backslash escaping in description-en test cases
    - Update assertion values to match expected escaped character behavior

--- tests/Unit/Infrastructure/Utils/SkillProjectConfigUtilTest.php
+++ tests/Unit/Infrastructure/Utils/SkillProjectConfigUtilTest.php
@@ -11,6 +11,9 @@ use Dtyq\SuperMagic\Domain\Skill\Entity\SkillEntity;
 use Dtyq\SuperMagic\Infrastructure\Utils\SkillProjectConfigUtil;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * @internal
+ */
 class SkillProjectConfigUtilTest extends TestCase
 {
     public function testBuildAndRenderConfigFromSkillEntity(): void
@@ -34,7 +37,7 @@ class SkillProjectConfigUtilTest extends TestCase
         self::assertStringContainsString('skill:', $yaml);
         self::assertStringContainsString('  dir: "my-skill"', $yaml);
         self::assertStringContainsString('  name-cn: "我的技能"', $yaml);
-        self::assertStringContainsString('  description-en: "English \\"description\\"\\nline2"', $yaml);
+        self::assertStringContainsString('  description-en: "English \"description\"\nline2"', $yaml);
     }
 
     public function testParseRenderedConfigRoundTripsEscapedValues(): void
@@ -43,13 +46,13 @@ class SkillProjectConfigUtilTest extends TestCase
             'skill:',
             '  dir: "renamed-skill"',
             '  name: "my-skill"',
-            '  description: "English \\"description\\"\\nline2"',
+            '  description: "English \"description\"\nline2"',
             '',
             '  name-cn: "中文名"',
             '  description-cn: "中文描述"',
             '',
             '  name-en: "English Name"',
-            '  description-en: "English \\\\ path"',
+            '  description-en: "English \\\ path"',
             '',
         ]);
 
@@ -58,7 +61,7 @@ class SkillProjectConfigUtilTest extends TestCase
         self::assertSame('renamed-skill', $config['skill']['dir']);
         self::assertSame("English \"description\"\nline2", $config['skill']['description']);
         self::assertSame('中文描述', $config['skill']['description-cn']);
-        self::assertSame('English \\ path', $config['skill']['description-en']);
+        self::assertSame('English \ path', $config['skill']['description-en']);
     }
 
     public function testValidateSkillDir(): void
