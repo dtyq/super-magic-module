670dbe5fd984cf41c40609bdc87c215cb835ca27
lihq1403 <lihqing1403@gmail.com>
2026-04-21 21:49:00 +0800

    fix(app): restore AppCodeEnum import in HandleTaskMessageAppService.php

--- src/Application/SuperAgent/Service/HandleTaskMessageAppService.php
+++ src/Application/SuperAgent/Service/HandleTaskMessageAppService.php
@@ -7,8 +7,8 @@ declare(strict_types=1);
 
 namespace Dtyq\SuperMagic\Application\SuperAgent\Service;
 
-use App\Application\LongTermMemory\Enum\AppCodeEnum;
 use App\Application\Kernel\EnvManager;
+use App\Application\LongTermMemory\Enum\AppCodeEnum;
 use App\Application\ModelGateway\Mapper\ModelGatewayMapper;
 use App\Domain\Chat\DTO\Message\Common\MessageExtra\SuperAgent\SuperAgentExtra;
 use App\Domain\Contact\Entity\MagicUserEntity;
